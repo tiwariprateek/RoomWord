83da54deacad3b14d825c4136b192eadf3bf05c2
Prateek Tiwari <prateektiwari5399@gmail.com>
2020-03-22 02:14:14 +0530

    Modified WordRepository Class

--- app/src/main/java/com/example/roomword/WordViewModel.kt
+++ app/src/main/java/com/example/roomword/WordViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.roomword
 
-class WordViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class WordViewModel(application: Application):AndroidViewModel(application) {
+    private val repository:WordRepository
+    val allWords:LiveData<List<Word>>
+    init {
+        val wordsDao=WordRoomDatabase.getDatabse(application).wordDao()
+        repository= WordRepository(wordsDao)
+        allWords=repository.allWords
+    }
+    fun insert(word: Word)=viewModelScope.launch {
+        repository.insert(word)
+    }
 }
