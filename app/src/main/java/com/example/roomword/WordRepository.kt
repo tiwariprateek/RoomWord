c7b94e1e2da57fe6f2f80545088c0b01668ad8d0
Prateek Tiwari <prateektiwari5399@gmail.com>
2020-03-22 01:46:13 +0530

    Added WordRepository Class

---
+++ app/src/main/java/com/example/roomword/WordRepository.kt
@@ -0,0 +1,4 @@
+package com.example.roomword
+
+class WordRepository {
+}
--- app/src/main/java/com/example/roomword/WordRoomDatabase.kt
+++ app/src/main/java/com/example/roomword/WordRoomDatabase.kt
@@ -2,6 +2,7 @@ package com.example.roomword
 
 import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = arrayOf(Word::class), version = 1,exportSchema = false )
@@ -12,7 +13,18 @@ public abstract class WordRoomDatabase :RoomDatabase() {
         @Volatile
         private var INSTANCE :WordRoomDatabase?=null
         fun getDatabse(context: Context):WordRoomDatabase{
-
+            val tempInstance = INSTANCE
+            if(tempInstance !=null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "word_database").build()
+                INSTANCE=instance
+                return instance
+            }
         }
     }
 }
