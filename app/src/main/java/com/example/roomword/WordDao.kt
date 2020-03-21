1a96b0676bbb88092cebe1e1de3120bf89f7e0be
Prateek Tiwari <prateektiwari5399@gmail.com>
2020-03-21 20:38:24 +0530

    Added DAO interface for queries

---
+++ app/src/main/java/com/example/roomword/WordDao.kt
@@ -0,0 +1,19 @@
+package com.example.roomword
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface WordDao {
+
+    @Query("Select * from word_table ORDER BY word ASC")
+    fun getAlphabetizedWords():List<Word>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE from word_table")
+    suspend fun deleteAll()
+}
